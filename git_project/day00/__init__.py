60defad16187a5b3db53156669d4e3de45586ef3
Mr1cao1 <448097950@qq.com>
2022-06-21 22:17:45 +0800

    提交helloworld

---
+++ git_project/day00/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2022/6/21 22:13
+Copyright (c) 2022/6/21, 海牛学院版权所有.
+@author: cs
+"""
---
+++ git_project/day00/hello_world.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""
+hello_world.py
+Created on 2022/6/21 22:14
+Copyright (c) 2022/6/21, 海牛学院版权所有.
+@author: cs
+"""
+print("hello world")
